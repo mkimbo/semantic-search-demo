29a46a47214a4178f8f703c44796a1971606a8cc
Jack <jackmkimbo@gmail.com>
2024-02-19 02:01:57 +0300

    minor updates

--- src/search.ts
+++ src/search.ts
@@ -1,30 +1,27 @@
-import Car from "./mongoose/car.js"
+import Car from "./mongoose/car.js";
 import generateEmbeddings from "./generateEmbeddings.js";
-
-
 
 export default async (searchQuery: string) => {
   const embeddings = await generateEmbeddings(searchQuery);
-  const docs = await Car.aggregate(
-    [
-      {
-        $search: {
-          index: "indexedCars",
-          knnBeta: {
-            vector: embeddings,
-            path: "embeddings",
-            k: 100,
-          },
+  const docs = await Car.aggregate([
+    {
+      $search: {
+        index: "indexedCars",
+        knnBeta: {
+          vector: embeddings,
+          path: "embeddings",
+          k: 100,
         },
       },
-      {
-        "$limit": 5
-      },
-      {
-        $project: { slug: 1, price: 1 },
-      },
-    ],
-  )
-  console.log(docs, "docs")
-  return docs;
-}
+    },
+    {
+      $limit: 5,
+    },
+    {
+      $project: { slug: 1, price: 1, make: 1, model: 1, year: 1 },
+    },
+  ]);
+  return {
+    searchResults: docs,
+  };
+};
